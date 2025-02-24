91080610ff19e879c8f71feb3de0ece37d66e69a
Hyung <hyung@letterboxd.com>
2025-02-24 13:47:08 +1300

    Conform models to Sendable in preparation for Swift 6

---
+++ .changeset/seven-ties-tickle.md
@@ -0,0 +1,5 @@
+---
+"@openapi-generator-plus/swift-client-generator": patch
+---
+
+Conform models to Sendable in preparation for Swift 6
--- src/index.ts
+++ src/index.ts
@@ -84,7 +84,7 @@ const RESERVED_WORDS = [
 	'else', 'enum', 'extension', 'fallthrough', 'false', 'fileprivate', 'final', 'for', 'func', 'get', 'guard', 'if', 'import', 'in', 'indirect', 'infix', 'init', 'inout',
 	'internal', 'is', 'lazy', 'left', 'let', 'mutating', 'nil', 'none', 'nonmutating', 'open', 'operator', 'optional', 'override', 'postfix', 'precedence', 'prefix', 'private',
 	'protocol', 'public', 'repeat', 'required', 'rethrows', 'return', 'right', 'self', 'set', 'static', 'struct', 'subscript', 'super', 'switch', 'throw', 'throws', 'true',
-	'try', 'typealias', 'unowned', 'var', 'weak', 'where', 'while', 'willSet',
+	'try', 'typealias', 'unowned', 'var', 'weak', 'where', 'while', 'willSet', 'Sendable', 'Hashable',
 	'LocalDate', 'LocalTime', 'OffsetDateTime', 'Decimal', 'String', 'Void', 'File', 'FormData',
 	'unknown', // for our enum cases
 	'RetryConfiguration', 'Configuration',
